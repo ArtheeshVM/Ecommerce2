12f47b784e35ca550c80a55dd4f65f260441007f
ArtheeshVM <varanartheesh@gmail.com>
2025-04-15 21:19:48 +0530

    commit-10

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import './Item.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Item.css";
+import { Link } from "react-router-dom";
 
 const Item = (props) => {
   return (
-    <div className='item'>
-      <img src="https://backend-vtav.onrender.com/images/product_1744692736461.png" alt="img" />
-        <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src="https://backend-vtav.onrender.com/images/product_1744692736461.png" alt="Nothing..." /></Link>
-        <p>{props.name}</p>
-        <div className="item-prices">
-            <div className="item-price-new">
-            ₹{props.new_price}
-            </div>
-            <div className="item-price-old">
-            ₹{props.old_price}
-            </div>
-        </div>
+    <div className="item">
+      {/* <img src="https://backend-vtav.onrender.com/images/product_1744692736461.png" alt="img" /> */}
+      {/* <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src="https://backend-vtav.onrender.com/images/" alt="Nothing..." /></Link> */}
+      <Link to={`/product/${props.id}`}>
+        <img
+          onClick={window.scrollTo(0, 0)}
+          src="https://backend-vtav.onrender.com/images/product_{$props.id}.png"
+          alt="Nothing..."
+        />
+      </Link>
+      <p>{props.name}</p>
+      <div className="item-prices">
+        <div className="item-price-new">₹{props.new_price}</div>
+        <div className="item-price-old">₹{props.old_price}</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Item
+export default Item;
